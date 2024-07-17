52dfbdd5da54b3cca30b08aa77616bdbdaeee77d
Pierre <pierre.nygard@gmail.com>
2024-07-17 15:08:53 +0200

    Part 1 cleanup

--- 2016/01/part1.js
+++ 2016/01/part1.js
@@ -1,21 +1,21 @@
 import _ from 'lodash'
 
 export default function solution (input) {
-    let instructions = _.split(input, ', ');
-    let [dir_x, dir_y] = [0, -1];
-    let at = [0,0];
+    let instructions = _.split(input, ', ')
+    let [dir_x, dir_y] = [0, -1]
+    let at = [0,0]
 
     while (instructions.length > 0) {
-        let next_ins = instructions.shift();
+        let next_ins = instructions.shift()
         let [_, next_dir, steps] = /([A-Z])(\d+)/.exec(next_ins);
 
         [dir_x, dir_y] = next_dir === 'R' 
             ? [-dir_y, dir_x]   // 90 degrees clockwise
             : [dir_y, -dir_x];  // 90 degrees counterclockwise
         
-        at[0] += dir_x * steps;
-        at[1] += dir_y * steps;
+        at[0] += dir_x * steps
+        at[1] += dir_y * steps
     }
 
-    return Math.abs(at[0]) + Math.abs(at[1]);
+    return Math.abs(at[0]) + Math.abs(at[1])
 } 
--- 2016/01/part2.js
+++ 2016/01/part2.js
@@ -1,29 +1,29 @@
 import _ from 'lodash'
 
 export default function solution (input) {
-    let instructions = _.split(input, ', ');
-    let [dir_x, dir_y] = [0, -1];
-    let at = { x: 0, y: 0 };
-    let seen = [[at['x'], at['y']]];
+    let instructions = _.split(input, ', ')
+    let [dir_x, dir_y] = [0, -1]
+    let at = { x: 0, y: 0 }
+    let seen = [[at['x'], at['y']]]
 
     while (instructions.length > 0) {
-        let next_ins = instructions.shift();
+        let next_ins = instructions.shift()
         let [_, next_dir, steps] = /([A-Z])(\d+)/.exec(next_ins);
 
         [dir_x, dir_y] = next_dir === 'R' 
             ? [-dir_y, dir_x]   // 90 degrees clockwise
-            : [dir_y, -dir_x];  // 90 degrees counterclockwise
+            : [dir_y, -dir_x]  // 90 degrees counterclockwise
         
         for (let i = 0; i < steps; i++) {
-            at['x'] += dir_x;
-            at['y'] += dir_y;
+            at['x'] += dir_x
+            at['y'] += dir_y
 
-            let exists = seen.some(pos => pos[0] === at['x'] && pos[1] === at['y']);
+            let exists = seen.some(pos => pos[0] === at['x'] && pos[1] === at['y'])
             if (exists) {
-                return Math.abs(at['x']) + Math.abs(at['y']);
+                return Math.abs(at['x']) + Math.abs(at['y'])
             }
 
-            seen.push([at['x'], at['y']]);
+            seen.push([at['x'], at['y']])
         }
     }
 

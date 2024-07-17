f5d227bb7cb01a7f5abccb3b40c1efdbe40e2f6e
Pierre <pierre.nygard@gmail.com>
2024-07-17 15:09:00 +0200

    Part 2 cleanup

--- 2016/02/part1.js
+++ 2016/02/part1.js
@@ -6,15 +6,16 @@ export default function solution (input) {
     let keyPad = [[1,2,3], [4,5,6], [7,8,9]]
 
     let code = _.reduce(input, (code, line) => {
-        let moves = _.split(line, '');
+        let moves = _.split(line, '')
         while (moves.length > 0) {
             let next_move = directions[moves.shift()]
             let newPosition = _.map(position, (value, index) => value + next_move[index])
+            
             if (newPosition[0] >= 0 && newPosition[0] < 3 && newPosition[1] >= 0 && newPosition[1] < 3)
                 position = newPosition;
         }
-        return code + keyPad[position[1]][position[0]];
+        return code + keyPad[position[1]][position[0]]
     }, '')
 
-    return parseInt(code, 10);
+    return parseInt(code, 10)
 } 
--- 2016/02/part2.js
+++ 2016/02/part2.js
@@ -12,7 +12,7 @@ export default function solution (input) {
     ]
 
     let code = _.reduce(input, (code, line) => {
-        let moves = _.split(line, '');
+        let moves = _.split(line, '')
         while (moves.length > 0) {
             let next_move = directions[moves.shift()]
             let newPosition = _.map(position, (value, index) => value + next_move[index])
@@ -20,8 +20,8 @@ export default function solution (input) {
             if (newPosition[0] >= 0 && newPosition[0] < 5 && newPosition[1] >= 0 && newPosition[1] < 5 && keyPad[newPosition[1]][newPosition[0]] !== '-')
                 position = newPosition;
         }
-        return code + keyPad[position[1]][position[0]];
+        return code + keyPad[position[1]][position[0]]
     }, '')
 
-    return code;
+    return code
 } 

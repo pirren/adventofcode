4fde0132ed5367af5df6140f1595653edc94f873
Pierre <pierre.nygard@gmail.com>
2024-07-21 01:47:51 +0200

    2016: day13 refactored bfs search

--- 2016/13/part2.js
+++ 2016/13/part2.js
@@ -2,5 +2,5 @@ import { BFSTraversal} from './BFSTraversal.js'
 
 export default function solution (input) {
     let bfs = new BFSTraversal(input)
-    return bfs.traverse([1,1], undefined, 50)
+    return bfs.traverse([1,1], [], 50)
 }

3a4f4d3904fb7b0e440b30a4e8d7a555158e7cb8
Pierre Nygård <pierre.nygard@else.se>
2024-11-10 18:00:15 +0100

    2015: day10 updated

--- 2015/10/lookAndSay.js
+++ 2015/10/lookAndSay.js
@@ -1,6 +1,6 @@
-export default function lookAndSay(sequence, iterations) {
-    for (let i = 0; i < iterations; i++) {
-        sequence = sequence.replace(/(.)\1*/g, (say) => say.length.toString() + say[0])
+export default function lookAndSay(sequence, n) {
+    for (let i = 0; i < n; i++) {
+        sequence = sequence.replace(/(.)\1*/g, say => say.length.toString() + say[0])
     }
     return sequence.length
 }

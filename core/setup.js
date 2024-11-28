f2ae989a0335a1e2d13e991a3ac7739ac712c351
Pierre Nygård <pierre.nygard@else.se>
2024-11-28 18:48:48 +0100

    added parsing lib to new solutions

--- core/setup.js
+++ core/setup.js
@@ -36,7 +36,16 @@ export default function setup({ year = null, day = null } = {}) {
     // -- Create code
     console.log(`Creating code for parts 1 and 2...`)
 
-    let func = `import _ from 'lodash'\n\nexport const metadata = {\n    "Puzzle Name": undefined\n}\n\nexport default function solution (input) {\n    return input\n}`
+    let func = `import _ from 'lodash'
+import { ints } from '../../lib/parsing.js'
+
+export const metadata = {
+    "Puzzle Name": undefined
+}
+    
+export default function solution (input) {
+    return input
+}`
     
     fs.writeFileSync(`${dir}/part1.js`, func)
     fs.writeFileSync(`${dir}/part2.js`, func)

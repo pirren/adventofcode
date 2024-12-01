00c7bb022b436215c872e11146d1ea0d08e8fc6e
Pierre <pierre.nygard@gmail.com>
2024-12-01 21:26:36 +0100

    updated setup script with templates

--- core/setup.js
+++ core/setup.js
@@ -8,13 +8,57 @@ import _ from 'lodash'
 import fs from 'fs'
 import chalk from 'chalk'
 
+const formatDay = day => _.padStart(day, 2, '0')
+
+function getTestFileTemplate(year) {
+    const template = `
+    // Description: Tests for Advent of Code ${year} solutions.
+    import test from '../core/test.js'
+
+    describe('advent of code ${year}', () => {
+    })`
+
+    return template
+        .trim().split('\n')
+        .map(line => line.trim())
+        .join('\n')
+}
+
+function addTestsFromTemplate(tests, { year = null, day = null } = {}) {
+    const template = `
+    it('day ${formatDay(day)}, part 1', async function() {
+        await test({ year: ${year}, day: ${day}, part: 1, expected: 12345 })
+    })
+    it('day ${formatDay(day)}, part 2', async function() {
+        await test({ year: ${year}, day: ${day}, part: 2, expected: 12345 })
+    })`
+    .replace(/^\n|\n$/g, '').trimEnd();
+
+    tests.splice(tests.length - 1, 0, template)
+}
+
+function getSolutionTemplate() {
+    return [
+        `import _ from 'lodash'`,
+        `import { ints } from '../../lib/parsing.js'`,
+        ``,
+        `export const metadata = {`,
+        `    "Puzzle Name": undefined`,
+        `}`,
+        ``,
+        `export default function solution (input) {`,
+        `    return input`,
+        `}`
+    ].join('\n')
+}
+
 export default function setup({ year = null, day = null } = {}) {
     if (!day || !year) {
         console.log(chalk.red('Provide a year and day.'))
         process.exit(1)
     }
 
-    let dayFormatted = _.padStart(day, 2, '0')
+    let dayFormatted = formatDay(day)
     
     let dir = `./${year}`
     if (!fs.existsSync(dir)) {
@@ -28,50 +72,31 @@ export default function setup({ year = null, day = null } = {}) {
     
     console.log(`Creating directories for ${year}.${dayFormatted}...`)
     
-    // Create input
+    // -- Create empty input file
     console.log(`Creating input file...`)
     fs.mkdirSync(dir)
     fs.writeFileSync(`${dir}/input.txt`, '12345')
     
-    // -- Create code
+    // -- Create code from template
     console.log(`Creating code for parts 1 and 2...`)
+    let dayTemplate = getSolutionTemplate()
 
-    let func = `import _ from 'lodash'
-import { ints } from '../../lib/parsing.js'
-
-export const metadata = {
-    "Puzzle Name": undefined
-}
+    fs.writeFileSync(`${dir}/part1.js`, dayTemplate)
+    fs.writeFileSync(`${dir}/part2.js`, dayTemplate)
     
-export default function solution (input) {
-    return input
-}`
-    
-    fs.writeFileSync(`${dir}/part1.js`, func)
-    fs.writeFileSync(`${dir}/part2.js`, func)
-    
-    // -- Create tests
+    // -- Create test file from template if it doesn't exist
     let testPath = `./${year}/test.js`
     if (!fs.existsSync(testPath)) {
         console.log(`Creating test file...`)
-        fs.writeFileSync(testPath, `// Description: Tests for Advent of Code ${year} solutions.
-import test from '../core/test.js'
-
-describe('advent of code ${year}', () => {
-})`)
+        const testFileTemplate = getTestFileTemplate(year);
+        fs.writeFileSync(testPath, testFileTemplate)
     }
 
+    // -- Add tests to test file from template
     console.log(`Creating unit tests...`)
     let tests = fs.readFileSync(testPath, 'utf-8').split('\n')
-    tests.splice(tests.length - 1, 0, 
-`    it('day ${dayFormatted}, part 1', async function() {
-        await test({ year: ${year}, day: ${day}, part: 1, expected: 12345 })
-    })`,
-`    it('day ${dayFormatted}, part 2', async function() {
-        await test({ year: ${year}, day: ${day}, part: 2, expected: 12345 })
-    })`)
+    addTestsFromTemplate(tests, { year, day })
     fs.writeFileSync(testPath, tests.join('\n'))
-    
 
     console.log(chalk.green('Setup complete.'))
 }

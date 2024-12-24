6594a93fc02e052777115e7c4236419e0597c1d8
Pierre <pierre.nygard@gmail.com>
2024-12-24 01:43:56 +0100

    2024: day7 pipe

--- 2024/07/part1.js
+++ 2024/07/part1.js
@@ -1,20 +1,47 @@
-import _ from 'lodash'
 import { ints } from '../../lib/parsing.js'
-import { validateExpression, defaultOps } from './validateExpression.js'
+import { pipe, sum } from '../../lib/utils.js'
 
 export const metadata = {
     "Puzzle Name": "Bridge Repair"
-}
+};
 
-export default function solution (input) {
-    const equations = input.map(equation => {
+const parseEquations = (input) => 
+    input.map(equation => {
         const [target, ...numbers] = ints(equation)
         return { target, numbers }
     });
 
-    let calibrationResult = _.sum(
-        equations.map(({ target, numbers }) => validateExpression(target, numbers, defaultOps))
-    );
+const operations = {
+    '+': (a, b) => a + b,
+    '*': (a, b) => a * b
+};
 
-    return calibrationResult;
-}
+const filter = (fn) => (input) => input.map(v => fn(v)).filter(Boolean);
+
+const calibrate = ({ target, numbers }) => {
+    let stack = [{ subtotal: numbers.at(0), index: 1 }]
+
+    while(stack.length) {
+        let { subtotal, index } = stack.pop()
+        if (subtotal > target) continue
+        
+        if (index == numbers.length) {
+            if (subtotal == target) return target
+            continue
+        }
+
+        let next = numbers[index]
+        for (const op of Object.keys(operations)) {
+            let newSubTotal = operations[op](subtotal, next) 
+            stack.push({ subtotal: newSubTotal, index: index + 1 })
+        }
+    }
+
+    return false;
+};
+
+export default pipe( 
+    parseEquations,
+    filter(calibrate),
+    sum
+);
